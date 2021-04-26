f985ed4cc5c4cfe7d5728c9394c0d25a9098e810
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-26 14:48:17 +0000

    (feat): create route methods

--- main.go
+++ main.go
@@ -17,8 +17,33 @@ type Book struct {
 
 // Author Struct
 type Author struct {
-	firstName string
-	lastName  string
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+// Get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Get one books
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Create new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Update book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Delete book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func main() {
@@ -27,7 +52,7 @@ func main() {
 
 	// Route Handlers / Endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
 	r.HandleFunc("/api/books", createBook).Methods("POST")
 	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
