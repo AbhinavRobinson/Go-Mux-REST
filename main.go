ccc70f9f3a76dcadbdd79e4e748f81429fa1b377
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-26 15:29:50 +0000

    (feat): add update method

--- main.go
+++ main.go
@@ -61,6 +61,21 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 // Update book
 func updateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, book := range books {
+		if book.ID == params["id"] {
+			// remove old entry
+			books = append(books[:index], books[index+1:]...)
+			// get new details
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = strconv.Itoa(rand.Intn(10000)) // Mock ID - not safe
+			// add book
+			books = append(books, book)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 // Delete book
