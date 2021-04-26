2ff618d77890c057d28267bfc4d61460f7f63b06
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-26 17:06:28 +0000

    (feat): abstract into proper MVC pattern

---
+++ app/controllers/controller.go
@@ -0,0 +1,38 @@
+package controllers
+
+import (
+	"net/http"
+
+	helper "github.com/abhinavrobinson/Go-Mux-REST/app/controllers/helpers"
+	"github.com/abhinavrobinson/Go-Mux-REST/app/models"
+)
+
+// Init Book slice
+var Books []models.Book
+
+// ------ DISPATCHERS ------
+
+// Get All Books
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	helper.GetBooks(w, r, Books)
+}
+
+// Get one books
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	helper.GetBook(w, r, Books)
+}
+
+// Create new book
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	Books = helper.CreateBook(w, r, Books)
+}
+
+// Update book
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	Books = helper.UpdateBook(w, r, Books)
+}
+
+// Delete book
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	Books = helper.DeleteBook(w, r, Books)
+}
--- app/main.go
+++ app/main.go
@@ -6,58 +6,28 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/abhinavrobinson/Go-Mux-REST/app/controllers"
+	store "github.com/abhinavrobinson/Go-Mux-REST/app/controllers"
 	"github.com/abhinavrobinson/Go-Mux-REST/app/models"
 	"github.com/gorilla/mux"
 )
-
-// Init Book slice
-var books []models.Book
 
 func main() {
 	// Init Router
 	r := mux.NewRouter()
 
 	// Mock Data - !todo - add database
-	books = append(books, models.Book{ID: "1", Isbn: "0001", Title: "Default Book A", Author: &models.Author{FirstName: "John", LastName: "Doe"}})
-	books = append(books, models.Book{ID: "2", Isbn: "0002", Title: "Default Book B", Author: &models.Author{FirstName: "Kevin", LastName: "Smith"}})
+	store.Books = append(store.Books, models.Book{ID: "1", Isbn: "0001", Title: "Default Book A", Author: &models.Author{FirstName: "John", LastName: "Doe"}})
+	store.Books = append(store.Books, models.Book{ID: "2", Isbn: "0002", Title: "Default Book B", Author: &models.Author{FirstName: "Kevin", LastName: "Smith"}})
 
 	// Route Handlers / Endpoints
-	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	r.HandleFunc("/api/books", createBook).Methods("POST")
-	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	r.HandleFunc("/api/books", store.GetBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", store.GetBook).Methods("GET")
+	r.HandleFunc("/api/books", store.CreateBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", store.UpdateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", store.DeleteBook).Methods("DELETE")
 
 	// Serve to port
 	const port int = 8000
 	fmt.Printf("Starting Server on port %d ...", port)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), r))
-}
-
-// ------ DISPATCHERS ------
-
-// Get All Books
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	controllers.GetBooks(w, r, books)
-}
-
-// Get one books
-func getBook(w http.ResponseWriter, r *http.Request) {
-	controllers.GetBook(w, r, books)
-}
-
-// Create new book
-func createBook(w http.ResponseWriter, r *http.Request) {
-	books = controllers.CreateBook(w, r, books)
-}
-
-// Update book
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	books = controllers.UpdateBook(w, r, books)
-}
-
-// Delete book
-func deleteBook(w http.ResponseWriter, r *http.Request) {
-	books = controllers.DeleteBook(w, r, books)
 }
